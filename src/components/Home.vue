a083f3b0b816a37a76256d2fff6eceed9e3d1cf9
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-25 22:21:52 +0900

    カスタムディレクティブ作成（ローカル登録）

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,3 +5,26 @@
     <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">Home</p>
   </div>
 </template>
+
+<script>
+export default {
+  // カスタムディレクティブのローカル登録
+  directives: {
+    border(el, binding) {
+      // オブジェクトで渡されたデータを反映する
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      //引数を取得
+      el.style.borderStyle = binding.arg;
+      // 修飾子にroundがある場合
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      // 修飾子にshadowがある場合
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+      }
+    }
+  }
+}
+</script>
