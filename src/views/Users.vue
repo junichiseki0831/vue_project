9080637bb7e2d748f45217dadea6b30ebe5850ff
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-31 21:41:20 +0900

    URLにクエリを付与する

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -3,7 +3,7 @@
     <h3>Users</h3>
     <hr>
     <h1>User No. {{ id }}</h1>
-    <router-link :to="{ name: users-id-profiles, params: { id: Number(id) + 1}}">次のユーザー</router-link>
+    <router-link :to="{ name: users-id-profiles, params: { id: Number(id) + 1}, query: {lang: 'ja', page:2}}">次のユーザー</router-link>
   </div>
 </template>
 
