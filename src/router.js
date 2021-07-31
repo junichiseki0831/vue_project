50d99463c382100a7ce018091b138d23dc7c424f
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-31 19:43:50 +0900

    childrenオプションでネストされたtoute-viewを表示する

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Users from './views/Users.vue';
+import UsersPosts from './views/UsersPosts.vue';
+import UsersProfile from './views/UsersProfile.vue';
 
 // Routerを使用する宣言
 Vue.use(Router);
@@ -13,7 +15,14 @@ export default new Router({
   //ルーティング先とコンポーネントをマッピングする
   routes: [
     {path: '/', component: Home}, 
-    {path: '/users/:id', component: Users, props: true }
+    { path: '/users/:id',
+      component: Users,
+      props: true,
+      children: [
+        {path: "posts", component: UsersPosts},
+        {path: "profile", component: UsersProfile},
+      ]
+    }
   ]
 });
 
