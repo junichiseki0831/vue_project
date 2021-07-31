5fa849c1962c7d0d09058a7b6bd438bb6d0a85e9
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-31 16:21:24 +0900

    router経由での画面切り替え

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <h3>Home</h3>
+    <button @click="toUsers">Usersへ</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toUsers() {
+      //router経由で画面の切り替え
+      this.$router.push('users');
+    }
+  }
+}
+</script>
