6e598a22e881e6780d733c589b16ffd14d49ddd6
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-31 16:16:58 +0900

    active-classとexactの使用（アクティブと非アクティブ切り替え

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/users">User</router-link>
+      <router-link to="/" active-class="link--active" exact>Home</router-link>
+      <router-link to="/users" active-class="link--active" exact>User</router-link>
     </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link--active {
+    font-size: 20px;
+  }
+</style>
