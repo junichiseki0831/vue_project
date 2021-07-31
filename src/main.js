892f925716b129d00a489488cb872f0714454b6b
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-07-31 14:04:24 +0900

    vue-routerインストールとrouter.js、テストページ作成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import LikeNumber from './components/LikeNumber.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 //グローバル登録
@@ -11,5 +12,7 @@ Vue.filter("upperCase", function(value) {
 });
 
 new Vue({
+  //インポートしたrouterを登録
+  router,
   render: h => h(App)
 }).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Users from './views/Users.vue';
+
+// Routerを使用する宣言
+Vue.use(Router);
+
+// Routerをエクスポートする
+export default new Router({
+  //ルーティング先とコンポーネントをマッピングする
+  routes: [{path: '/', component: Home}, {path: '/users', component: Users}]
+});
+
