8f412a0e6beb58056b3f00d44e1f88566a2d7f04
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-08-01 22:11:22 +0900

    mutationsを効率よく取得するためのmapMutationsを使用する

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,20 +3,17 @@
     <h2>HomeHeader</h2>
     <router-link to="/" active-class="link--active" exact>Home</router-link>
     <router-link to="/users" active-class="link--active" exact>User</router-link>
-    <button @click='increment'>+1</button>
-    <button @click='decrement'>-1</button>
+    <button @click='increment(2)'>+1</button>
+    <button @click='decrement(2)'>-1</button>
   </nav>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   methods: {
-    increment() {
-      this.$store.commit("increment", 2);
-    },
-    decrement() {
-      this.$store.commit("decrement", 2);
-    }
+    ...mapMutations(["increment", "decrement"])
   }
 }
 </script>
