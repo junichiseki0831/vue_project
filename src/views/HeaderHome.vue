958be58d586b11811c81cfc1d26b0787f7f5b519
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-08-03 20:52:36 +0900

    mapActionsで効率よくactionsをコンポーネントで使用する

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -3,23 +3,25 @@
     <h2>HomeHeader</h2>
     <router-link to="/" active-class="link--active" exact>Home</router-link>
     <router-link to="/users" active-class="link--active" exact>User</router-link>
-    <button @click='increment'>+1</button>
-    <button @click='decrement'>-1</button>
+    <button @click='increment(2)'>+1</button>
+    <button @click='decrement(2)'>-1</button>
   </nav>
 </template>
 
 <script>
 // import { mapMutations } from "vuex";
+import { mapActions } from "vuex";
 
 export default {
   methods: {
     // ...mapMutations(["increment", "decrement"])
-    increment() {
-      this.$store.dispatch('increment', 2)   
-    },
-    decrement() {
-      this.$store.dispatch('decrement', 2)   
-    }
+    ...mapActions(["increment", "decrement"])
+    // increment() {
+    //   this.$store.dispatch('increment', 2)   
+    // },
+    // decrement() {
+    //   this.$store.dispatch('decrement', 2)   
+    // }
   }
 }
 </script>
