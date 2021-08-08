1bda1a634deff868387cc56921bd245a95a57341
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2021-08-08 13:59:32 +0900

    【ログイン機能作成】登録画面のURLをカスタムインスタンス化と登録メソッド作成しユーザー登録を可能にする

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //カスタムインスタンス（axiosの機能を受け継いで変更したい箇所は変更できる）
 const instance = axios.create({
-  baseURL: "https://firestore.googleapis.com/v1/projects/vuejs-http-38403/databases/(default)/documents"
+  baseURL: "https://identitytoolkit.googleapis.com/v1"
 });
 
 export default instance;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+import axios from "../axios-auth";
+
 export default {
   data() {
     return {
@@ -20,7 +22,18 @@ export default {
     };
   },
   methods: {
-    register() {}
+    register() {
+      axios.post(
+        '/accounts:signUp?key=AIzaSyDj85gVn62w18mQYwvuGV9Ve3vLwHyKoO4',
+        {
+          email: this.email,
+          password: this.password,
+          returnSecureToken: true
+        }
+      ).then(response =>{
+        console.log(response);
+      });
+    }
   }
 }
 </script>
